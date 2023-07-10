c31ff6ee6555bbe775ab429108e42e98ba2440f8
Tatyana Larina <jerrymurr@ya.ru>
2023-07-10 20:18:30 +0300

    task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+
+    return (arr1.length != arr2.length) ? false : arr1.every(index => arr1[index] === arr2[index]);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => {
+		acc += age;
+		if (index === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+    }, 0);
+
 }
