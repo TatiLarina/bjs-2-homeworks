bc63caeaa2e2b2d35419c25c6cff98ba3f8ba491
Tatyana <108877033+TatiLarina@users.noreply.github.com>
2023-07-21 12:42:52 +0300

    task 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(number) {
+	if (Number.isNaN(Number.parseFloat(number))) {
+		throw new Error("Невалидное значение");
+	}
+	return Number.parseFloat(number);
+}
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (((a + b) < c) || ((c + b) < a) || ((a + c) < b)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		let p = 1 / 2 * (this.a + this.b + this.c);
+		let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return Number(s.toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}
